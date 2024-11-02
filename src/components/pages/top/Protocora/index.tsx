b7b37a170b31e439f205f1f005decf5a61b6ea95
mikio（岡村美紀） <>
2024-11-02 23:41:52 +0900

    feat: 🎸 add modal

--- src/components/pages/top/Protocora/index.tsx
+++ src/components/pages/top/Protocora/index.tsx
@@ -1,6 +1,18 @@
+'use client'
+import { useState } from 'react'
 import style from './index.module.scss'
 
 export const Protocora = () => {
+    const [selectedImageIndex, setSelectedImageIndex] = useState<number | null>(null)
+
+    const openModal = (index: number) => {
+        setSelectedImageIndex(index)
+    }
+
+    const closeModal = () => {
+        setSelectedImageIndex(null)
+    }
+
     return (
         <section id="protocora" >
             <div className={style.protocora}>
@@ -8,15 +20,25 @@ export const Protocora = () => {
                     <img src="/assets/protocora/main_prt.png" alt="" className={style.mainProtocora}/>
                 </div>
                 <div className={style.bg}>
-                    <h1>Protocora</h1>
                     <div className={style.wrapper}>
-                        {/* <img src="/assets/protocora/itiran.JPG" alt="" /> */}
+                        <img src="/assets/protocora/Types-of-protocola.png" alt="" className={style.ptr_ttl}/>
                         <div className={style.prts}>
                         {[...Array(8)].map((_, i) => {
                             return (
-                                <div key={i} className={style.prt}>
-                                    <img src={`/assets/protocora/prts/prt0${i + 2}.png`} alt="" />
-                                </div>
+                                <>
+                                    <div key={i} className={style.prt}>
+                                        <button onClick={() => openModal(i)} className={style.buttonStyle}>
+                                            <img src={`/assets/protocora/prts/prt0${i + 2}.png`} alt="" />
+                                        </button>
+                                    </div>
+                                    {selectedImageIndex === i && (
+                                        <div className={style.modal}>
+                                            <button onClick={closeModal} className={style.modal}>
+                                                <img src={`/assets/protocora/ex/ex0${i + 2}.png`} alt="" className={style.modal_img}/>
+                                            </button>
+                                        </div>
+                                    )}
+                                </>
                             )
                         })}
                         </div>
