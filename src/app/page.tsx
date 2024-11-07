7d9b0bb7585b3686cadaed0ecaef7136fd2a9140
mikio（岡村美紀） <>
2024-11-07 14:35:29 +0900

    feat: 🎸 add google search console

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -11,6 +11,7 @@ export default function Home() {
     <>
     <Head>
       <meta property="og:image" content="/assets/kv-intro/main.png" />
+      <meta name="google-site-verification" content="gzsjjE8sS-VsJqAJhglfnVGZqYlyT55UgCt5xOkJhR0" />
       <link rel="preconnect" href="https://fonts.googleapis.com" />
       <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="" />
       <link href="https://fonts.googleapis.com/css2?family=Shippori+Mincho:wght@400;500;600;700;800&display=swap" rel="stylesheet" />
