be36ed14b6aa2808bba534f9ca5eac27557e8426
mikio（岡村美紀） <hellomiki2027@gmail.com>
2024-11-19 04:34:53 +0900

    fix: 🐛 xml

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,5 +1,6 @@
 module.exports = {
     siteUrl: "https://halo-iland.github.io",
     generateRobotsTxt: true,
-    outDir: './out'
+    outDir: './out',
+    xmlNs: 'xmlns="http://www.sitemaps.org/schemas/sitemap/0.9"',
   };
